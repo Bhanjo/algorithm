bc2df1f3655740209f08d34ad58245ed30b6cd05
Bhanjo <hanjo1515@naver.com>
2021-06-25 16:29:29 +0900

    0625_백준15596번

---
+++ backjoon_step6/back15596.py
@@ -0,0 +1,15 @@
+# 풀이
+def solve(a: list) :
+    result = sum(a)
+    return print(result)
+
+def main() :
+    a = list(map(int, input().split()))
+    solve(a)
+if __name__ == "__main__" :
+    main()
+
+# 제출용
+def solve(a):
+    ans = sum(a)
+    return ans
