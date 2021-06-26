36448ac17dfa099e89967aa01497d04089d7e271
Bhanjo <hanjo1515@naver.com>
2021-06-26 22:17:29 +0900

    0626_백준1065번

---
+++ backjoon_step6/back1065.py
@@ -0,0 +1,18 @@
+def hansu() :
+    num = int(input())
+    hansu = 0
+
+    for i in range(1, num+1) :
+        if i <= 99 :
+            hansu += 1
+        else :
+            ckList = list(map(int, str(i)))
+            if ckList[0] - ckList[1] == ckList[1] - ckList[2] :
+                hansu += 1
+    
+    print(hansu)
+
+def main() :
+    hansu()
+if (__name__ == "__main__") :
+    main()
