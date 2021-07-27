086bd675bb5fd03cac22d30058b8b2de12635f21
Bhanjo <hanjo1515@naver.com>
2021-07-27 18:27:23 +0900

    백준 1018번

--- backjoon_step11/back1018.py
+++ backjoon_step11/back1018.py
@@ -6,14 +6,16 @@ def myCode():
     for _ in range(x):
         ches.append(input())
 
+
+    # 처음이 w일때, b일때 나눔
     for a in range(x-7):
         for b in range(y-7):
-            cnt1 = 0
-            cnt2 = 0
+            cnt1 = 0 # 처음이 w일때 판별
+            cnt2 = 0 # 처음이 b일때 판별
             for i in range(a, a+8):
                 for j in range(b, b+8):
                     if(i + j) % 2 == 0:
-                        if(ches[i][j] != 'W') : cnt1 += 1
+                        if(ches[i][j] != 'W') : cnt1 += 1 # w가 아니면 처음이 w가 아닌게 됨(+1)
                         if(ches[i][j] != 'B') : cnt2 += 1
                     else :
                         if(ches[i][j] != 'B') : cnt1 += 1
