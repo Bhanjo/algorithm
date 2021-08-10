0b267db9f6ea678268fd6a65f77193e18347dc4a
Bhanjo <hanjo1515@naver.com>
2021-08-10 13:50:54 +0900

    백준 15649번

--- backjoon_step14/back15649.py
+++ backjoon_step14/back15649.py
@@ -6,16 +6,19 @@ def myCode():
     arr = []
 
     def DFS(x):
+        # 입력받은 숫자가 끝 까지 오게 된다면 출력
         if x == b:
             print(*arr)
         
         for i in range(a):
-            if check_num[i]:
+            if check_num[i]: # 이미 방문한 숫자라면 넘어감
                 continue
+            # i를 배열에 추가 후 해당 위치 방문 표시
             arr.append(num_list[i])
             check_num[i] = True
-            DFS(x+1)
-            arr.pop()
+            
+            DFS(x+1) # 다음 DFS 실행
+            arr.pop() # 조건이 True 되기 직전인 상태로 되돌림
             check_num[i] = False # check 초기화
     DFS(0)
 
