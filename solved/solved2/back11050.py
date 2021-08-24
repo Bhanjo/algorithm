d271b8d1969a2c9d429d9615e29d1e4f5e79042d
Bhanjo <hanjo1515@naver.com>
2021-08-24 14:05:26 +0900

    백준 11050번

---
+++ solved/solved2/back11050.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+def facto(x):
+    hap = 1
+    for i in range(1,x+1):
+        hap *= i
+    return hap
+
+child = facto(n)
+parant1 = facto(k)
+parant2 = facto(n-k)
+print(child//(parant1*parant2))
