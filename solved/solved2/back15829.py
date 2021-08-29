e65a9b6ccc606fe83d04574307c38aa04420372d
Bhanjo <hanjo1515@naver.com>
2021-08-29 22:41:56 +0900

    리팩토링: 백준 15829번 리팩토링

--- solved/solved2/back15829.py
+++ solved/solved2/back15829.py
@@ -1,15 +1,8 @@
 n = int(input())
-en = str(input())
-hash = []
-r = 31
-m = 1234567891
+en = input()
 
 # 알파벳 소문자는 97~122
-# 부분성공 범위 1~50에서의 최적화 필요
-for i in range(len(en)):
-    hash.append(ord(en[i])-96)
 result = 0
 for j in range(n):
-    result += hash[j] * (r**j) % m
-
-print(result)
+    result += (ord(en[j])-96) * (31**j)
+print(result % 1234567891)
