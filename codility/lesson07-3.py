e19290f3baf5714006f807c85a9051b6d35f6567
Bhanjo <hanjo1515@naver.com>
2021-09-03 18:11:43 +0900

    코딜리티 레슨07-3

---
+++ codility/lesson07-3.py
@@ -0,0 +1,12 @@
+def solution(S):
+    cnt = 0
+    for i in S:
+        if i == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt < 0:
+            return 0
+
+    return 1 if cnt == 0 else 0
+    pass
