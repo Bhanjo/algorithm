db11153241ba87c71e5419ced31c951dac0317f8
Bhanjo <hanjo1515@naver.com>
2021-09-29 21:49:22 +0900

    수정: 백준 2606번 방문노드 로직 변경

--- solved/solved3/back2606.py
+++ solved/solved3/back2606.py
@@ -14,14 +14,13 @@ cnt = 0
 
 def dfs(graph, start, visited=[1]):
     global cnt
+    visited.append(start)
     for node in graph[start]:
         if node not in visited:
             cnt += 1
-            visited.append(node)
             dfs(graph, node, visited)
 
     return cnt
 
 
-dfs(graph, 1)
-print(cnt)
+print(dfs(graph, 1))
