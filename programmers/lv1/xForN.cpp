dff42310d2a1866e601a04fd33f295279f475408
Bhanjo <hanjo1515@naver.com>
2022-02-23 11:21:21 +0900

    lv1.x만큼 간격이 있는 n개의 숫자

---
+++ programmers/lv1/xForN.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// vector<long long> solution(int x, int n) {
+//   vector<long long> answer;
+//   for (int i = 1; i <= n; i++) {
+//     answer.push_back(x*i);
+//   }
+//   return answer;
+// }
+
+int main() {
+  // solution(10, 1000);
+  cout << "hello";
+  return 0;
+}
