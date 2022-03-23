5cc57b335d40b21e897c6625b0fb667eff952e54
Bhanjo <hanjo1515@naver.com>
2022-03-23 16:40:33 +0900

    백준 1927번

---
+++ solved/solved3/back1927.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+h = []
+for i in range(n):
+    num = int(input())
+    if num == 0:
+        if len(h) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(h))
+    else:
+        heapq.heappush(h, num)
