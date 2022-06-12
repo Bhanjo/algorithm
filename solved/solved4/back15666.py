2e09168efeaf7355522076cfdac342aa41281dc1
Bhanjo <hanjo1515@naver.com>
2022-06-12 17:25:29 +0900

    백준 15666번

--- solved/solved4/back15666.py
+++ solved/solved4/back15666.py
@@ -4,20 +4,18 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 nums.sort()
-visit = [0 for _ in range(n)]
 ans = []
 
-def DFS(count): 
+def DFS(count, visit):
     if count == m:
         print(' '.join(map(str, ans)))
         return
     checkNum = 10001
     for i in range(n):
-        if visit[i] == 0 and checkNum != nums[i]:
-            visit[i] = 1
-            checkNum = nums[i]
+        if visit <= nums[i] and checkNum != nums[i]:
             ans.append(nums[i])
-            DFS(count + 1)
-            visit[i] = 0
+            checkNum = nums[i]
+            DFS(count + 1, nums[i])
             ans.pop()
-DFS(0)
+
+DFS(0, -1)
