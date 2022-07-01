a05ae2e2843d4cc1ce9bdb9c3bd5905d20516feb
Bhanjo <hanjo1515@naver.com>
2022-07-01 22:13:03 +0900

    lv1 숫자 문자열과 영단어 복습

--- programmers/lv1/숫자문자열과영단어.py
+++ programmers/lv1/숫자문자열과영단어.py
@@ -35,3 +35,22 @@ def solution(s):
     return int(''.join(answer))
 
 solution("oneseveneightfoursixeight")
+
+# 2회차
+def solution(s):
+    answer = ''
+    word = ''
+    alphabets = {
+        'zero': '0', 'one': '1', 'two': '2', 'three': '3',
+        'four': '4', 'five': '5', 'six': '6', 'seven': '7',
+        'eight': '8', 'nine': '9'
+    }
+    for i in s:
+        if '0' <= i <= '9':
+            answer += i
+        else:
+            word += i
+        if word in alphabets:
+            answer += alphabets[word]
+            word = ''
+    return int(answer)
