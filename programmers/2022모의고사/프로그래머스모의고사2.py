9f773d0966d10e5bbb21a62b074bbf11bc9ca8ae
Bhanjo <hanjo1515@naver.com>
2022-07-21 16:01:48 +0900

    모의고사2

--- programmers/2022모의고사/프로그래머스모의고사2.py
+++ programmers/2022모의고사/프로그래머스모의고사2.py
@@ -1,3 +1,4 @@
+# 1회차
 def solution(want, number, discount):
     answer = 0
     food = {}
@@ -26,4 +27,45 @@ def solution(want, number, discount):
             if food[want[i]] == 0 and i == len(want) - 1:
                 answer += 1
 
+    return answer
+
+# 2회차
+def solution(want, number, discount):
+    answer = 0
+    foods = {}
+
+    # 딕셔너리 초기값 세팅
+    for i in range(len(number)):
+        foods[want[i]] = number[i]
+    
+    # 1일차 세팅
+    for i in range(10):
+        if discount[i] in foods:
+            foods[discount[i]] -= 1
+    # 회원가입 가능 판별
+    newFoods = list(foods.items())
+    for j in range(len(newFoods)):
+        if newFoods[j][1] != 0:
+            break
+        if j == len(want)-1:
+            if newFoods[j][1] == 0:
+                answer += 1
+    
+    # 2일차 이후 반복
+    for i in range(1, len(discount) - 9):
+        # 이전값 되돌리기
+        if discount[i-1] in foods:
+            foods[discount[i-1]] += 1
+        # 끝값 넣기
+        if discount[i+9] in foods:
+            foods[discount[i+9]] -= 1
+        
+        # 회원가입 가능 판별
+        newFoods = list(foods.items())
+        for j in range(len(newFoods)):
+            if newFoods[j][1] != 0:
+                break
+            if j == len(want)-1:
+                if newFoods[j][1] == 0:
+                    answer += 1
     return answer
