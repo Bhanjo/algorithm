e165cec01415ca5df81b6d46725c2fc26eb7f74a
Bhanjo <hanjo1515@naver.com>
2022-07-28 01:25:59 +0900

    백준 11444번 코드 최적화

--- solved/solved4/back11444.py
+++ solved/solved4/back11444.py
@@ -5,33 +5,14 @@ n = int(input())
 graph = [[1,1],[1,0]]
 
 # x*x
-def calcEven(graph):
+def calcEven(a,b):
     tempGraph = [[0,0],[0,0]]
     for i in range(2):
         for j in range(2):
             for k in range(2):
-                tempGraph[i][j] += graph[i][k] * graph[k][j]
+                tempGraph[i][j] += a[i][k] * b[k][j]
             tempGraph[i][j] %= 1000000007
     return tempGraph
-
-# x*x*graph
-def calcOdd(graph):
-    tempGraph = [[0,0],[0,0]]
-    originGraph = [[1,1],[1,0]]
-    for i in range(2):
-        for j in range(2):
-            for k in range(2):
-                tempGraph[i][j] += graph[i][k] * graph[k][j]
-            tempGraph[i][j] %= 1000000007
-
-    ansGraph = [[0,0],[0,0]]
-    for i in range(2):
-        for j in range(2):
-            for k in range(2):
-                ansGraph[i][j] += tempGraph[i][k] * originGraph[k][j]
-            ansGraph[i][j] %= 1000000007
-    
-    return ansGraph
 
 def divcon(graph, n):
     if n == 1:
@@ -39,9 +20,9 @@ def divcon(graph, n):
     else:
         x = divcon(graph, n // 2)
         if n % 2 == 0:
-            return calcEven(x)
+            return calcEven(x,x)
         else:
-            return calcOdd(x)
+            return calcEven(calcEven(x,x), graph)
 
 answer = divcon(graph, n)
 print(answer[0][1] % 1000000007)
