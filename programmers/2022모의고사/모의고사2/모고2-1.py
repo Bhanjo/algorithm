bec8f989d3ba371e9f376a3f79065652e6a89d6d
Bhanjo <hanjo1515@naver.com>
2022-07-28 16:48:26 +0900

    프로그래머스 2회 모의고사 1번

---
+++ programmers/2022모의고사/모의고사2/모고2-1.py
@@ -0,0 +1,14 @@
+# 100/100
+def solution(number):
+    answer = 0
+    visit = []
+    for i in range(len(number)-2):
+        for j in range(i+1, len(number)-1):
+            for k in range(j+1, len(number)):
+                hap = number[i] + number[j] + number[k]
+                arr = [i,j,k]
+                arr.sort()
+                if hap == 0 and arr not in visit:
+                    visit.append(arr)
+                    answer += 1
+    return answer
