d7e1562d485491607bcf22e821b0aad6db50e743
Bhanjo <hanjo1515@naver.com>
2022-08-04 18:40:55 +0900

    프로그래머스 모고 2-1 복습

--- programmers/2022모의고사/모의고사2/모고2-1.py
+++ programmers/2022모의고사/모의고사2/모고2-1.py
@@ -12,3 +12,16 @@ def solution(number):
                     visit.append(arr)
                     answer += 1
     return answer
+
+
+# 2회차 (100/100)
+def solution(number):
+    answer = 0
+    for i in range(len(number)-2):
+        for j in range(i+1, len(number)-1):
+            for k in range(j+1, len(number)):
+                hap = number[i] + number[j] + number[k]
+                if hap == 0:
+                    answer += 1
+
+    return answer
