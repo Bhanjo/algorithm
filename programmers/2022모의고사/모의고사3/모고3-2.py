4812638dbea07a187cbb1f9444ca17071a39bf3f
Bhanjo <hanjo1515@naver.com>
2022-08-16 16:27:11 +0900

    프로그래머스 모의고사 3-2

---
+++ programmers/2022모의고사/모의고사3/모고3-2.py
@@ -0,0 +1,15 @@
+#  100 / 100
+def solution(ingredient):
+    answer = 0
+    st = []
+    for i in ingredient:
+        st.append(i)
+        # 마지막이 빵, 대기열 = 4
+        if len(st) >= 4 and st[-1] == 1:
+            temp = st[len(st)-4:len(st)]
+            if temp == [1,2,3,1]:
+                answer += 1
+                for i in range(4):
+                    st.pop()
+
+    return answer
