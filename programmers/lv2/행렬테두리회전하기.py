0245a1fe2fd3618c5382659325f82b5809bc35c6
Bhanjo <hanjo1515@naver.com>
2022-08-19 14:59:55 +0900

    lv2 행렬 테두리 회전하기

--- programmers/lv2/행렬테두리회전하기.py
+++ programmers/lv2/행렬테두리회전하기.py
@@ -33,3 +33,46 @@ def solution(rows, columns, queries):
         answer.append(minValue)
         
     return answer
+
+# 2회차
+def solution(rows, columns, queries):
+    answer = []
+    graph = []
+    
+    # 그래프 제작
+    cnt = 1
+    for i in range(rows):
+        col = []
+        for j in range(columns):
+            col.append(cnt)
+            cnt += 1
+        graph.append(col)
+    
+    for x1, y1, x2, y2 in queries:
+        minVal = 10**7
+        temp = graph[x1-1][y1-1]
+        # 왼쪽 세로
+        for i in range(x1-1, x2-1):
+            val = graph[i][y1-1]
+            minVal = min(minVal, val)
+            graph[i][y1-1] = graph[i+1][y1-1]
+        # 아래 가로
+        for i in range(y1-1, y2-1):
+            val = graph[x2-1][i]
+            minVal = min(minVal, val)
+            graph[x2-1][i] = graph[x2-1][i+1]
+        # 오른쪽 세로
+        for i in range(x2-1, x1-1, -1):
+            val = graph[i][y2-1]
+            minVal = min(minVal, val)
+            graph[i][y2-1] = graph[i-1][y2-1]
+        # 위 가로
+        for i in range(y2-1, y1-1, -1):
+            val = graph[x1-1][i]
+            minVal = min(minVal, val)
+            graph[x1-1][i] = graph[x1-1][i-1]
+        
+        graph[x1-1][y1] = temp
+        answer.append(minVal)
+    
+    return answer
