e46ca9d294ec23a4e7aca6a3e65c3cfbbdeb0cd8
Bhanjo <hanjo1515@naver.com>
2022-08-22 21:40:50 +0900

    lv2 순위검색 복습

--- programmers/lv2/순위검색.py
+++ programmers/lv2/순위검색.py
@@ -28,7 +28,7 @@
 #     return answer
 
 
-# 이진 탐색 (효율 테스트 탈락)
+# 이진 탐색
 from bisect import bisect_left
 from itertools import combinations
 
@@ -72,6 +72,84 @@ def solution(info, query):
         else:
             answer.append(0)
 
+    return answer
+
+# 2회차
+def solution(info, query):
+    answer = []
+    infos = []
+    
+    # info를 리스트로
+    for h in info:
+        infos.append(h.split(" "))
+    
+    # 각 쿼리문 실행
+    for q in query:
+        qu = q.split(" and ")
+        a = qu.pop()
+        qu.extend(a.split(" "))
+        temp = infos[:]
+        cnt = 0
+        # 쿼리 문 각 요소 실행(점수 조건 전까지만)
+        for idx in range(len(qu)-1):
+            filterArr = []
+            if qu[idx] == '-':
+                continue
+            for inf in range(len(temp)):
+                if qu[idx] == temp[inf][idx]:
+                    filterArr.append(temp[inf])
+            temp = filterArr # 필터링 된 것으로 값 갱신
+        # 점수 판별
+        temp.sort(key=lambda x:int(x[4]))
+        # 이진 탐색 실행 (배열, 찾을 값)
+        for i in temp:
+            if int(i[4]) >= int(qu[-1]):
+                cnt += 1
+        answer.append(cnt)
+    return answer
+
+# 2회차 이진탐색
+from itertools import combinations
+from bisect import bisect_left
+
+def solution(info, query):
+    answer = []
+    dir = {}
+    
+    # info각 요소를 가지고 모든 경우의 수 만들어 딕셔너리에 저장
+    for h in info:
+        infos = h.split(" ")
+        value = int(infos.pop())
+        for i in range(5):
+            for combi in combinations(infos, i):
+                combi = ''.join(map(str, combi))
+                # print(combi)
+                if combi not in dir:
+                    dir[combi] = [value]
+                else:
+                    dir[combi].append(value)
+    
+    # 이진탐색을 위한 정렬
+    for i in dir:
+        dir[i].sort()
+    
+    for q in query:
+        condi = q.split(" ")
+        value = int(condi.pop())
+        # and와 -제거
+        while("and" in condi):
+            condi.remove("and")
+        while("-" in condi):
+            condi.remove("-")
+        condi = ''.join(condi)
+        if condi in dir:
+            nums = dir[condi]
+            if nums:
+                idx = bisect_left(nums, value)
+                answer.append(len(nums)-idx)
+        else:
+            answer.append(0)
+    
     return answer
 
 solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"])
