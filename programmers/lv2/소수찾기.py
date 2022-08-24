3e0797ae2a050d5c33b15a92baef86d0bf3d8fd1
Bhanjo <hanjo1515@naver.com>
2022-08-25 00:26:28 +0900

    lv2 소수 찾기

--- programmers/lv2/소수찾기.py
+++ programmers/lv2/소수찾기.py
@@ -24,4 +24,43 @@ def solution(numbers):
     permuList = [''.join(n) for n in permu]
     return isPrime(permuList)
 
+# 2회차 (permutation 직접 구현 1회차 비교 6369ms -> 22ms 단축)
+answer = 0
+def solution(numbers):
+    global answer
+    numbers = list(numbers)
+    visit = [False for _ in range(len(numbers))]
+    check = {}
+    temp = []
+    
+    def isPrime(n):
+        if n <= 1:
+            return False
+        if n == 2:
+            return True
+        for i in range(2, int(n**(1/2))+1):
+            if n % i == 0:
+                return False
+        return True
+    
+    def dfs():
+        global answer
+        num = ''.join(temp)
+        if num != '':
+            num = int(num)
+        if num != '' and num not in check:
+            check[num] = True
+            if isPrime(num):
+                answer += 1
+        for i in range(len(numbers)):
+            if not visit[i]:
+                visit[i] = True
+                temp.append(numbers[i])
+                dfs()
+                temp.pop()
+                visit[i] = False
+    
+    dfs()
+    return answer
+
 print(solution("011"))
