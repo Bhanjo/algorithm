66d31bd5ae56e1182528b46a67cb940b1d0c062a
Bhanjo <hanjo1515@naver.com>
2022-08-25 13:44:10 +0900

    lv3 가장 먼 노드

--- programmers/lv3/가장먼노드.py
+++ programmers/lv3/가장먼노드.py
@@ -26,3 +26,29 @@ def solution(n, edge):
             answer += 1
     
     return answer
+
+# 2회차
+from collections import deque
+def solution(n, edge):
+    graph = [[] for _ in range(n+1)]
+    visit = [0 for _ in range(n+1)]
+    
+    for i in edge:
+        a, b = i[0], i[1]
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    def bfs(node):
+        q = deque()
+        q.append(node)
+        visit[node] += 1
+        
+        while(q):
+            parent = q.popleft()
+            for child in graph[parent]:
+                if visit[child] == 0:
+                    visit[child] = visit[parent] + 1
+                    q.append(child)
+    
+    bfs(1)
+    return visit.count(max(visit))
