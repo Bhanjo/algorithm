84eed0d22f2b812f6f3c5bdfefae5aba4b67cf3f
Bhanjo <hanjo1515@naver.com>
2022-09-02 14:42:57 +0900

    백준 2504번 복습

--- backjoon/highscore_kit/back2504.py
+++ backjoon/highscore_kit/back2504.py
@@ -42,3 +42,29 @@ if len(stack) != 0:
     answer = 0
 
 print(answer)
+
+# 2회차
+n = list(input().strip())
+st = []
+temp = 1
+answer = 0
+
+for i, braket in enumerate(n):
+    if braket == '(' or braket =='[':
+        st.append('(' if braket == '(' else '[')
+        temp = temp * 2 if braket == '(' else temp * 3
+    
+    if braket == ')' or braket == ']':
+        if len(st) == 0:
+            answer = 0
+            break
+        b = st.pop()
+        if (b == '(' and braket == ')') or (b == '[' and braket == ']'):
+            if n[i-1] == '(' or n[i-1] == '[':
+                answer += temp
+            temp = temp // 2 if b == '(' else temp // 3
+        else:
+            answer = 0
+            break
+
+print(answer if len(st) == 0 else 0)
