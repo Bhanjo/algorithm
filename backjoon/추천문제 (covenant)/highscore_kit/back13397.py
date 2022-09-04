343241e762f50e9836d3dc4024f37fc2723f8baa
Bhanjo <hanjo1515@naver.com>
2022-09-04 15:17:07 +0900

    백준 13397번 복습

--- backjoon/추천문제 (covenant)/highscore_kit/back13397.py
+++ backjoon/추천문제 (covenant)/highscore_kit/back13397.py
@@ -11,7 +11,7 @@ answer = right
 def divSearch(mid):
     global answer
     minVal, maxVal = nums[0], nums[0]
-    div = 1
+    div = 1 # 아무것도 안 자르면 배열은 1개이기 때문
 
     # 투 포인터로 최대 최소 구하고 판별
     for i in nums:
@@ -21,20 +21,22 @@ def divSearch(mid):
             minVal = i
         # 구간 나누기
         if maxVal - minVal > mid:
-            # i 이전 인덱스까지를 한 구간으로 함
+            # i 기준으로 두 개로 분리 (구간 생성)
             div += 1
             # i를 시작 인덱스로 지정
             minVal = i
             maxVal = i
     
-    # 정해진 횟수만큼 나눌 수 있을 때
+    # 정해진 구간 이하로 나눌 수 있을 때
     return m >= div
 
 while(left <= right):
     mid = (left + right) // 2
     isValid = divSearch(mid)
-    # m개로 나눌 수 있는지 판별
+
+    # m개 이하로 나눌 수 있는지 판별
     if isValid:
+        # 값이 '최소'인 것을 골라야함, mid로 m개 이하로 나눌 수 있으면 더 작은 값으로 시도해보기
         right = mid - 1
         answer = min(answer, mid)
     else:
