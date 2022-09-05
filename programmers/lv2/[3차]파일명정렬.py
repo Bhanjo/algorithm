44814cbf80460c789b15e801f876306cb53ef9d4
Bhanjo <hanjo1515@naver.com>
2022-09-05 17:41:54 +0900

    lv2 파일명 정렬

--- programmers/lv2/[3차]파일명정렬.py
+++ programmers/lv2/[3차]파일명정렬.py
@@ -32,3 +32,40 @@ def solution(files):
     
     return [''.join(s) for s in sort]
 solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"])
+
+# 2회차
+def solution(files):
+    answer = []
+    temp = []
+    
+    for file in files:
+        # originHead, head, number, originNumber, tail
+        div = ['','','','','']
+        numCnt = 0
+        
+        for i, word in enumerate(file):
+            if not word.isdigit() and len(div[2]) == 0: # head 판별
+                div[0] += word
+                div[1] += word
+            elif word.isdigit(): # number 판별
+                if numCnt < 5:
+                    div[2] += word
+                    div[3] += word
+                    numCnt += 1
+                if numCnt >= 5: # 나머지 tail
+                    div[4] += file[i+1:]
+                    break
+            else: # 나머지 tail
+                div[4] += file[i:]
+                break
+        
+        # 소문자, 숫자 변환
+        div[1] = div[1].lower()
+        div[2] = int(div[2])
+        temp.append(div)
+    
+    temp.sort(key=lambda x:(x[1],x[2]))
+    for i in temp:
+        answer.append(i[0] + i[3] + i[4])
+    
+    return answer
