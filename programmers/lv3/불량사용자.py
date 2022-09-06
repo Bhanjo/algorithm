b394b8de6316bfc96f7c584b7ab18ecdaeca569e
Bhanjo <hanjo1515@naver.com>
2022-09-06 22:33:27 +0900

    lv3 불량 사용자 복습

--- programmers/lv3/불량사용자.py
+++ programmers/lv3/불량사용자.py
@@ -41,3 +41,53 @@ def solution(user_id, banned_id):
     dfs()
     
     return len(answer)
+
+
+# 2회차
+import copy
+
+def solution(user_id, banned_id):
+    answer = []
+    limit = len(banned_id)
+    temp = []
+    visit = [False for _ in range(len(user_id))]
+    
+    def isConsist(temp):    
+        # temp 목록을 전부 *로 대체 (기준: 벤idx의 요소)
+        newTemp = []
+        for idx, user in enumerate(temp):
+            newUser = list(user)
+            if len(user) != len(banned_id[idx]):
+                return False
+            for i in range(len(banned_id[idx])):
+                if banned_id[idx][i] == '*':
+                    newUser[i] = '*'
+            newTemp.append(''.join(newUser))
+        
+        if newTemp != banned_id:
+            return False
+    
+        return True
+    
+    def permu():
+        if len(temp) == limit:
+            # 벤 아이디와 일치하는지 판별
+            isTrue = isConsist(temp)
+            if isTrue:
+                newTemp = copy.deepcopy(temp)
+                newTemp.sort()
+                newTemp = list(set(newTemp))
+                if len(newTemp) == len(banned_id) and newTemp not in answer:
+                    answer.append(newTemp)
+            return
+        for idx in range(len(user_id)):
+            if not visit[idx]:
+                temp.append(user_id[idx])
+                visit[idx] = True
+                permu()
+                visit[idx] = False
+                temp.pop()
+    
+    permu()
+    
+    return len(answer)
