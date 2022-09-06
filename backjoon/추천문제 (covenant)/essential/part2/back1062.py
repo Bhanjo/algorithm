74a4cc442f4c2d825bdbbb637d3697be91a70589
Bhanjo <hanjo1515@naver.com>
2022-09-06 14:17:09 +0900

    백준 1062번 복습

--- backjoon/추천문제 (covenant)/essential/part2/back1062.py
+++ backjoon/추천문제 (covenant)/essential/part2/back1062.py
@@ -44,3 +44,51 @@ def permu(target, cnt):
 
 permu(0,0)
 print(answer)
+
+
+# 2회차
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+n,k = map(int, input().split())
+answer = 0
+words = []
+base = ['a','n','t','i','c']
+
+# 공통 단어 제거
+for i in range(n):
+    word = input().rstrip()
+    for i in base:
+        word = word.replace(i,'')
+    words.append(word)
+
+k -= 5
+if k < 0:
+    print(0)
+else:
+    alpha = []
+    for word in words:
+        if word == '': continue
+        al = list(set(word))
+        alpha.extend(al)
+    alpha = list(set(alpha))
+    combis = []
+    
+    # 남은 단어로 k-5길이의 조합 만들 수 있는지 확인 (만들 수 없으면 최대 길이로 만들기)
+    if len(alpha) < k:
+        combis.extend(list(combinations(alpha, len(alpha))))
+    else:
+        combis.extend(list(combinations(alpha, k)))
+
+    # 조합의 요소로 단어 지우고 단어가 다 지워졌는지 판별
+    for combi in combis:
+        temp = 0
+        for word in words:
+            for c in combi:
+                word = word.replace(c,'')
+            if word == '':
+                temp += 1
+        answer = max(answer, temp)
+
+    print(answer)
