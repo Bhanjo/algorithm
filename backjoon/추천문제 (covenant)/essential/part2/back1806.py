df45b823b97b95f34d20bc202977d102b53384c7
Bhanjo <hanjo1515@naver.com>
2022-09-07 17:26:56 +0900

    백준 1806번 복습

--- backjoon/추천문제 (covenant)/essential/part2/back1806.py
+++ backjoon/추천문제 (covenant)/essential/part2/back1806.py
@@ -22,3 +22,27 @@ while(left < len(nums)):
         temp += nums[right]
 
 print(answer if isExist else 0)
+
+# 2회차
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+nums = list(map(int, input().split()))
+
+left,right = 0,0
+hap = nums[0]
+answer = 10**6
+
+while(left <= m and right <= m):
+    if hap >= m:
+        answer = min(answer, right - left + 1)
+        hap -= nums[left]
+        left += 1
+    elif hap < m:
+        right += 1
+        if right >= n:
+            break
+        hap += nums[right]
+
+print(answer if answer != 10**6 else 0)
