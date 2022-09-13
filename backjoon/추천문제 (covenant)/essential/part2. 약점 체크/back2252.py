1752ab2d3b55763c645fcb77f7500125a7b114f3
Bhanjo <hanjo1515@naver.com>
2022-09-13 13:58:39 +0900

    백준 2252번 복습

--- backjoon/추천문제 (covenant)/essential/part2. 약점 체크/back2252.py
+++ backjoon/추천문제 (covenant)/essential/part2. 약점 체크/back2252.py
@@ -26,5 +26,35 @@ def topologySort():
                 q.append(i)
 
 topologySort()
+print(*answer)
 
+# 복습
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+graph = [[] for _ in range(n)]
+edges = [0 for _ in range(n)]
+answer= []
+
+for i in range(m):
+    start, end = map(int, input().split())
+    graph[start-1].append(end-1)
+    edges[end-1] += 1
+
+def topolSort():
+    q = deque()
+    for idx, edge in enumerate(edges):
+        if edge == 0:
+            q.append(idx)
+    while(q):
+        node = q.popleft()
+        answer.append(node+1)
+        for i in graph[node]:
+            edges[i] -= 1
+            if edges[i] == 0:
+                q.append(i)
+
+topolSort()
 print(*answer)
