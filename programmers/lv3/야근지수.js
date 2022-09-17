35f48cfc40ea1a782e04b5dc22e79532d8f19b74
Bhanjo <hanjo1515@naver.com>
2022-09-17 13:28:59 +0900

    lv3 야근 지수 복습

--- programmers/lv3/야근지수.js
+++ programmers/lv3/야근지수.js
@@ -20,3 +20,28 @@ function solution(n, works) {
 
   return answer;
 }
+
+// 2회차
+function solution(n, works) {
+  var answer = 0;
+  works.sort((a, b) => b - a);
+  let maxVal = works[0];
+
+  let sum = works.reduce((hap, x) => hap + x, 0);
+  if (sum <= n) return 0;
+
+  while (n > 0) {
+    for (let i = 0; i < works.length; i++) {
+      if (n <= 0) break;
+      if (works[i] == maxVal) {
+        works[i] -= 1;
+        n -= 1;
+      }
+    }
+    maxVal -= 1;
+  }
+
+  for (let work of works) answer += work ** 2;
+
+  return answer;
+}
