83afb942f02fad4ec2bde915b2986ac07741531b
Bhanjo <hanjo1515@naver.com>
2023-12-28 00:02:06 +0900

    lv1 3진법 뒤집기 js 코드개선

--- programmers/lv1/3진법뒤집기.js
+++ programmers/lv1/3진법뒤집기.js
@@ -1,19 +1,14 @@
 function solution(n) {
   var answer = [];
 
-  while (n > 1) {
-    answer.push(n % 3);
+  while (n > 0) {
+    answer.unshift(n % 3);
     n = Math.floor(n / 3);
-    console.log(n);
   }
 
-  if (n > 0) answer.push(n);
-
-  let cnt = 1;
-  let hap = 0;
-  while (answer.length > 0) {
-    hap += cnt * answer.pop();
-    cnt *= 3;
-  }
+  const hap = answer.reduce(
+    (prev, value, idx) => prev + value * Math.pow(3, idx),
+    0
+  );
   return hap;
 }
